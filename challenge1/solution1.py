e01cc5afcfae614e2d6849ff5ee25db1dc455905
GrayW <gray@live.co.uk>
2016-09-04 14:14:08 +0100

    Adds extra credit solution

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -6,3 +6,9 @@ reddit_username = input('Please tell me your Reddit Username: ')
 # Print the information in requested format
 print('Your name is ' + name + ', You are ' + age + ' years old, \
 and your username is ' + reddit_username)
+
+# Extra Credit: Save information to file for use later
+user_info = (name, age, reddit_username)
+save_user_info = str(user_info)
+file = open('userinfo.txt', 'w')
+file.write(save_user_info)
