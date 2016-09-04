f2b3d88a18ab41e8472777e31a0a7095f695eb40
GrayW <gray@live.co.uk>
2016-09-04 13:52:18 +0100

    Adds solution for Challenge 1

---
+++ challenge1/solution1.py
@@ -0,0 +1,8 @@
+# Capture information
+name = input('Please tell me your name: ')
+age = input('Please tell me your age: ')
+reddit_username = input('Please tell me your Reddit Username: ')
+
+# Print the information in requested format
+print('Your name is ' + name + ', You are ' + age + ' years old, \
+and your username is ' + reddit_username)
